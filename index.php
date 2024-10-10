38e1f6f5157c9db9fe77ab137a8f641ef909dd50
Dekbo TV <119120362+dekbotv@users.noreply.github.com>
2024-10-10 23:51:00 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Live TV | Sports</title>
+    <link rel="stylesheet" href="https://live.dekbotv.com/assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="heading">Live Sports Match</h1>
+        <p class="sub-heading" >Watch Live Sports for FREE</p>
+    </div>
+  
+   <a href="https://live.dekbotv.com/assets/links/live.html">
+        <div class="card">
+            <p class="des">Live Match</p>
+            <div class="card-des">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdJLKt7BWRIjFtCTU9BebFOvk0G0tSXb09pA&s" alt="" class="t1">
+                <img src="https://tvs6.github.io/sports_2.0/img/pngwing.com.png" alt="" class="vs">
+                <img src="https://t3.ftcdn.net/jpg/06/12/34/84/360_F_612348476_k3i3brMUOo3N9Wk3yoceMHqZ8w4jpFqu.jpg" alt="" class="t2">
+            </div>
+            <p class="live">LIVE</p>
+        </div>
+    </a>
+
+    <a href="https://live.dekbotv.com/assets/links/icc-w-t20.html">
+        <div class="card">
+            <p class="des">ICC Women's T20 World Cup</p>
+            <div class="card-des">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/2024_ICC_Women%27s_T20_World_Cup_qualifier_logo.jpg" alt="" class="t1">
+                <img src="https://tvs6.github.io/sports_2.0/img/pngwing.com.png" alt="" class="vs">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/2024_ICC_Women%27s_T20_World_Cup_qualifier_logo.jpg" alt="" class="t2">
+            </div>
+            <p class="live">LIVE</p>
+        </div>
+    </a>
+
+    <a href="https://live.dekbotv.com/assets/links/premier_League.html">
+        <div class="card">
+            <p class="des">Premier League</p>
+            <div class="card-des">
+                <img src="https://ssl.gstatic.com/onebox/media/sports/logos/5D811WOqeV3SRG6qt-DDPA_64x64.png" alt="" class="t1">
+                <img src="https://tvs6.github.io/sports_2.0/img/pngwing.com.png" alt="" class="vs">
+                <img src="https://ssl.gstatic.com/onebox/media/sports/logos/5D811WOqeV3SRG6qt-DDPA_64x64.png" alt="" class="t2">
+            </div>
+            <p class="live">LIVE</p>
+        </div>
+    </a>
+    
+
+    <a href="https://live.dekbotv.com/assets/links/laliga.html">
+        <div class="card">
+            <p class="des">LaLiga</p>
+            <div class="card-des">
+                <img src="https://tvs6.github.io/sports_2.0/img/la-liga-2016-2023-logo.png" alt="" class="t1">
+                <img src="https://tvs6.github.io/sports_2.0/img/pngwing.com.png" alt="" class="vs">
+                <img src="https://tvs6.github.io/sports_2.0/img/la-liga-2016-2023-logo.png" alt="" class="t2">
+            </div>
+            <p class="live">LIVE</p>
+        </div>
+    </a>
+
+    <a href="https://live.dekbotv.com/assets/links/Serie_A.html">
+        <div class="card">
+            <p class="des">Serie A</p>
+            <div class="card-des">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqWmu4-F0Vki2R74_BPKzFj6ecsGmHYfibKbKdV2zkOj5m4U_YjdBK6yyL&s=10" alt="" class="t1">
+                <img src="https://tvs6.github.io/sports_2.0/img/pngwing.com.png" alt="" class="vs">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqWmu4-F0Vki2R74_BPKzFj6ecsGmHYfibKbKdV2zkOj5m4U_YjdBK6yyL&s=10" alt="" class="t2">
+            </div>
+            <p class="live">LIVE</p>
+        </div>
+    </a>
+
+<!--
+<div class="counter">
+     <a href='https://www.versicherungen.at/tier-krankenversicherung/'>Tier-Krankenversicherung</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=c2c906da30d34d7a5b59379a0bcdee112037ae24'></script>
+<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1243702/t/1"></script>
+</div>
+--->    
+    
+    
+    
+   <script disable-devtool-auto src='https://cdn.jsdelivr.net/npm/disable-devtool@latest'></script>
+
+</body>
+</html>
